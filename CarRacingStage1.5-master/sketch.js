c17351d842537abe00ecf6a4550e73716c869b2c
sanskrutisontakke <81802614+sanskrutisontakke@users.noreply.github.com>
2021-09-17 15:06:15 +0530

    Add files via upload

---
+++ CarRacingStage1.5-master/sketch.js
@@ -0,0 +1,56 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+
+
+
+function preload(){
+
+ car_1 = loadImage('images/car1.png')
+ car_2 = loadImage('images/car2.png')
+ car_3 = loadImage('images/car3.png')
+ car_4 = loadImage('images/car4.png')
+
+ track = loadImage('images/track.png')
+ ground = loadImage('images/ground.png')
+
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState === 2){
+    
+    game.end()
+    
+
+
+  }
+
+
+
+
+}
